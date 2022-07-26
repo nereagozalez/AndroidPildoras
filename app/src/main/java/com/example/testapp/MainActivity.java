73b5b485a83d801ae75f60963a3c66584571a219
NereaGonzálezSanchez <nerea.gonzalez.sanchez@gmail.com>
2022-07-26 19:29:01 +0200

    Views and graphics IIb

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.content.Intent;
@@ -18,22 +19,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView( new MyOwnView( this ) );
+        //Colores y trazos con clases  Path y Color
     }
 
     //crear vista propia
-    public class MyOwnView View{
+    public class MyOwnView extends View{
         public MyOwnView(Context cntx){
             super( cntx );
         }
 
         @Override
         protected void onDraw(Canvas canvas){
-            Paint pencil = new Paint();
-            pencil.setColor( Color.BLUE );
+            Paint pencil = new Paint(); //creamos pincel
+            //pencil.setColor( Color.BLUE ); //tambien se puede poner en exadecimal
+            //pencil.setColor(Color.argb(125, 167,222, 25)); //otra forma de dar color
             pencil.setStrokeWidth( 8 );
             pencil.setStyle( Paint.Style.FILL );
 
+            //dibujar circulo con el pincel especificado
             canvas.drawCircle(175, 175, 100, pencil);
+            //la mejor forma es manejar colores con recursos -> values/color.xml -> <color name="white">#FFFFFFFF</color>
+            int myColor;
+            myColor = ContextCompat.getColor(getContext(),R.color.white);
+            pencil.setColor( myColor ); //color de recursos
         }
     }
 }
